ce427da588d2c597736560d5145259d96472da8f
estanionis <estanionis@gmail.com>
2026-05-25 14:40:31 +0300

    Fix flake8 E302 PEP8 style issue

--- service/routes.py
+++ service/routes.py
@@ -56,6 +56,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
